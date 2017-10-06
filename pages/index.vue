114281ce280c13049c9d8d15170990ec3bab90ef
Syed Aman <sna274@nyu.edu>
2017-10-06 10:46:04 -0500

    update GitHub repo link to the repo hosting hte app

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       </h3>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://github.com/SyedAman" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
